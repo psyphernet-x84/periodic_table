db3f1c2e945a6ff841c7e3461153eb1280cc9c84
psyphernet-x84 machine <okehiloe@gmail.com>
2024-10-03 12:55:11 +0000

    feat: main program

--- element.sh
+++ element.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+MAIN_PROGRAM() {
+  if [[ -z $1 ]]
+  then
+    echo "Please provide an element as an argument."
+  else
+    PRINT_ELEMENT $1
+  fi
+}
+
+MAIN_PROGRAM
